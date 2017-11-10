a562943e8831eb427f9668568b7a1a631d951266
Bianca Dennis <bianca.dennis@caa.columbia.edu>
2017-11-09 19:29:48 -0500

    searches. need to update table on click and update styling

--- index.js
+++ index.js
@@ -1,38 +1,45 @@
 $(document).ready(function() {
-    var quakes
+    function quakeSort(array) {
+        array.sort(function(a, b) {
+                return a["mag"] - b["mag"] || a["depth"] - b["depth"];
+            })
+        array.reverse()
+        array.splice(20)
+        return array
+    }
     $.ajax({
         url: "http://interviewtest.getguru.com/seismic/data.json",
         method: 'GET',
+        
         success: function(response) {
             quakes = response
-            // quakes.sort(function(a, b){
-            //     var a1= a.mag, b1= b.mag;
-            //     if(a1== b1) return 0;
-            //     return a1 < b1? 1: -1;
-            // });
-            quakes.sort(function(a, b) {
-                return a["mag"] - b["mag"] || a["depth"] - b["depth"];
-            });
-            quakes.reverse()    
-        }
-    }).then(function(){
-            console.log(quakes)
+            quakeSort(quakes);
             quakes.forEach(function(quake) {
                 $('table tbody').append(`<tr>
                                     <td>${quake.id}</td>
-                                    <td> ${quake.time}</td>
+                                    <td> ${moment(quake.time).format("MMM Do YYYY @ h:mm:ss a")}</td>
                                     <td> ${quake.place}</td>
                                     <td> ${quake.mag}</td>
-                                    <td> Holder </td>
+                                    <td><button type="button" onclick="$('#details').text('Longitude:${quake.longitude} and Latitude:${quake.latitude}')">Details</button> </td>
                                     </tr>`)
             })
-        })
-    // function listQuakes () {
-    //     console.log('hi')
-    //     quakes.forEach(function(element) {
-    //         $('#quakelist').html( "<li>Hello</li>")
-    //     })
-    // }
-    // listQuakes()
+            console.log(quakes)
 
+            $('#searchButton').click( function(event){
+            var queriedQuakes = []
+            var search     = $('#searchQuery');
+            var query = search.val();
+	        // var condition = `%${query}%`;
+            newRegex = new RegExp(query, 'g');
+            quakes.forEach(function(quake){
+                    result = newRegex.test(quake.place.toLowerCase());
+                    if (result) {
+                        queriedQuakes.push(quake);
+                    }
+            })
+            quakeSort(queriedQuakes);
+            console.log(queriedQuakes)
+            })
+        }
+    })
 })
